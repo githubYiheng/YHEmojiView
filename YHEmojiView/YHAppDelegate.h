1b5104183564bb4e42e599afa85634baa9c8c357
yiheng5959 <yihengmen@gmail.com>
2014-05-16 17:23:01 +0800

    yiheng first commit

---
+++ YHEmojiView/YHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YHAppDelegate.h
+//  YHEmojiView
+//
+//  Created by 一横 on 14-5-16.
+//  Copyright (c) 2014年 **. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YHEmojiView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  YHEmojiView
+//
+//  Created by 一横 on 14-5-16.
+//  Copyright (c) 2014年 **. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YHAppDelegate class]));
+    }
+}
